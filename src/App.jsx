9e1614664cdaf4c5258bad7f89d460a741671d03
jk-nishad-72 <jkn18869@gmail.com>
2025-09-17 23:22:23 +0530

    App.js formated

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import React ,{useRef}from 'react'
-import {Link, Route, Routes } from 'react-router'
+import React, { useRef } from 'react'
+import { Link, Route, Routes } from 'react-router'
 import Home from './pages/Home'
 import Agence from './pages/Agence'
 import Projects from './pages/Projects'
@@ -13,20 +13,14 @@ const App = () => {
 
   return (
     <div >
-
-
-
       < Navbar />
+      <FullScreenNabar />
+      <Routes>
+        <Route path='/' element={< Home />} />
+        <Route path='/agence' element={< Agence />} />
+        <Route path='/projects' element={< Projects />} />
 
-       <FullScreenNabar />
-
-       <Routes>
-        <Route path='/' element = {< Home/>} />
-        
-        <Route path='/agence' element = {< Agence/>} />
-        <Route path='/projects' element = {< Projects />} />
-        
-       </Routes>
+      </Routes>
 
     </div>
   )
